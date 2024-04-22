b0de6dea6127c80c2c67408c007e73ab923c1046
SMNoushan <noushan.md@gmail.com>
2024-04-22 13:19:26 +0600

    added footer

---
+++ src/components/shared/FooTer.jsx
@@ -0,0 +1,9 @@
+const FooTer = () => {
+  return (
+    <footer className="py-3 text-center text-white text-xl font-rancho bg-[url('/assets/images/more/24.jpg')] bg-center bg-cover bg-no-repeat">
+      <p>Copyright Espresso Emporium ! All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default FooTer;
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "../components/shared/NavBar";
-import Home from "../pages/home/Home";
+import FooTer from "../components/shared/FooTer";
 
 const Root = () => {
   return (
     <div>
       <NavBar />
-      <Home />
+      <div className="min-h-[calc(100dvh-196.58px)] font-raleway">
+        <Outlet />
+      </div>
+      <FooTer />
     </div>
   );
 };
