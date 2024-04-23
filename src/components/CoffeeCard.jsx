c4162371fa667f87f45be9ee4f904fd15d96aad0
SMNoushan <noushan.md@gmail.com>
2024-04-23 06:36:49 +0600

    added sweet alert for coffee details deletion

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,51 @@
 import { FaRegEye, FaPen, FaTrash } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import "animate.css";
 
 const CoffeeCard = () => {
+  const handleDelete = () => {
+    const swalWithCustomButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-error text-white",
+        cancelButton: "btn btn-info text-white ml-4",
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithCustomButtons
+      .fire({
+        title: "Are you sure?",
+        text: "Are you sure that you want to delete it?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        showClass: {
+          popup: `
+          animate__animated
+          animate__fadeInUp
+          animate__faster
+        `,
+        },
+        hideClass: {
+          popup: `
+          animate__animated
+          animate__fadeOutDown
+          animate__faster
+        `,
+        },
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Coffee Details Deleted",
+            icon: "success",
+          });
+        }
+      });
+    console.log(alert);
+  };
   return (
     <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:items-center p-[30px] bg-white-4f1 rounded-[10px]">
       <div>
@@ -26,9 +70,9 @@ const CoffeeCard = () => {
         <Link className="bg-dark-93b">
           <FaPen />
         </Link>
-        <Link className="bg-red-744">
+        <button onClick={() => handleDelete()} className="bg-red-744">
           <FaTrash />
-        </Link>
+        </button>
       </div>
     </div>
   );
